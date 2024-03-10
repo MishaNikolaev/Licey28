635dd90f8834dd969c1345809fd7dd9513dd04a6
MishaNikolaev <nmichail154@mail.ru>
2024-03-10 21:53:26 +0700

    clear code

--- app/src/main/java/com/example/yourlicey28/presentation/images/UsersViewModel.kt
+++ app/src/main/java/com/example/yourlicey28/presentation/images/UsersViewModel.kt
@@ -68,7 +68,7 @@ class UsersViewModel @Inject constructor(private val repository: RepositoryTask)
 
     fun generateRandomString(length: Int): String {
         val charPool: List<Char> =
-            ('a'..'z') + ('A'..'Z') + ('0'..'9') // Диапазон символов для генерации случайной строки
+            ('a'..'z') + ('A'..'Z') + ('0'..'9')
         return (1..length)
             .map { Random.nextInt(0, charPool.size) }
             .map(charPool::get)
